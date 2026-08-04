400ac93c4e7649d01f94b2cd7994c3413c5b33e7
ProBimkim <bimokimi@gmail.com>
2026-08-04 13:24:29 +0700

    update fix login

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,17 +1,10 @@
 import { betterAuth } from "better-auth";
-import { nextCookies } from "better-auth/next-js";
 import Database from "better-sqlite3";
 
-const db = new Database("sqlite.db");
+const database = new Database("auth.db");
 
 export const auth = betterAuth({
-  database: db,
-  secret: process.env.BETTER_AUTH_SECRET,
-  baseURL: process.env.BETTER_AUTH_URL,
-  emailAndPassword: {
-    enabled: true,
-  },
-  plugins: [
-    nextCookies(), // must be the last plugin
-  ],
+  database: database,
+  baseURL: "http://localhost:3000/",
+  emailAndPassword: { enabled: true },
 });
